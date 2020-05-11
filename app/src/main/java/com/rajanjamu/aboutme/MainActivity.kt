37da5b22c27eefbfe84cccc3385d06237498ad50
rajanjamu <rajanjamu@gmail.com>
2020-05-11 14:32:09 +0530

    Data Binding: Data

--- app/src/main/java/com/rajanjamu/aboutme/MainActivity.kt
+++ app/src/main/java/com/rajanjamu/aboutme/MainActivity.kt
@@ -15,9 +15,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName(name="Rajan Gupta")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         binding.doneButton.setOnClickListener {
             addNickname(it)
@@ -26,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View) {
         binding.apply {
-            binding.nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             binding.nicknameEdit.visibility = View.GONE
             binding.doneButton.visibility = View.GONE
